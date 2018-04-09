2fc823c3f93ecef207b3cb48673d2bedd3e6bf00
Felicien FOTIO <felicien.fotiomanfo@gmail>
2018-04-09 16:34:42 +0100

    Domain types/classes set up in progress ...

---
+++ notifications/app/NotifyWorkflow.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: felic
+ * Date: 4/9/2018
+ * Time: 2:49 PM
+ */
+
+    require_once('Domain.php');
+
+    function notify($event, $subscriptionList, $notificationProvider){
+
+        return new NotificationResult();
+    }
+
+?>
